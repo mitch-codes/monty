6c8e300da81f6070f8679860ebbd9d49b4d3dab6
mitch codes <mitchkodes@gmail.com>
2022-06-15 16:32:30 -0700

    monty interpreter

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: always 1
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+}
